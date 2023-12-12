a5c04cdfdc8d6483a800cc04021d71406ec901cd
Matias Rolón <matiasrolon1301@gmail.com>
2023-12-12 16:52:27 -0300

    Load with scroll

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,15 +1,48 @@
-import React, { useState, useEffect,  } from 'react';
-import { IonContent, IonPage, IonRow, IonCol, IonGrid, } from '@ionic/react';
+import React, { useState, useEffect, useCallback } from 'react';
+import {
+  IonContent,
+  IonPage,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
+} from '@ionic/react';
 import { getPokemonList } from '../services/api';
 import PokemonItem from './PokemonItem';
 
 const PokemonList: React.FC = () => {
   const [pokemonList, setPokemonList] = useState<any[]>([]);
+  const [offset, setOffset] = useState(0);
+  const limit = 10;
+  const [disableInfiniteScroll, setDisableInfiniteScroll] = useState(false);
 
   const loadPokemon = async () => {
-    const data = await getPokemonList(0, 10);
+    const data = await getPokemonList(offset, limit);
     setPokemonList((prevList) => [...prevList, ...data.results]);
+    setOffset((prevOffset) => prevOffset + limit);
   };
+
+  const loadMorePokemon = useCallback(
+    async (event:any) => {
+      if (disableInfiniteScroll) {
+        event.target.complete();
+        return;
+      }
+
+      await loadPokemon();
+      setTimeout(() => {
+        event.target.complete();
+
+        // Check if there are more items to load
+        if (pokemonList.length >= 150) {
+          setDisableInfiniteScroll(true);
+        }
+      }, 500);
+    },
+    [disableInfiniteScroll, loadPokemon, pokemonList.length]
+  );
+
   useEffect(() => {
     const fetchData = async () => {
       await loadPokemon();
@@ -20,16 +53,23 @@ const PokemonList: React.FC = () => {
   return (
     <IonPage>
       <IonContent>
-      <IonGrid>
+        <IonGrid>
           <IonRow>
             {pokemonList.map((_: any, index) => (
               <IonCol size="12" size-md="6" size-lg="4" key={index}>
-                <PokemonItem id={1} />
+                <PokemonItem id={index + offset + 1} />
               </IonCol>
             ))}
           </IonRow>
         </IonGrid>
-        </IonContent>
+        <IonInfiniteScroll
+          threshold="100px"
+          disabled={disableInfiniteScroll}
+          onIonInfinite={(e: CustomEvent<void>) => loadMorePokemon(e)}
+        >
+          <IonInfiniteScrollContent loadingText="Cargando más Pokémon..."></IonInfiniteScrollContent>
+        </IonInfiniteScroll>
+      </IonContent>
     </IonPage>
   );
 };
